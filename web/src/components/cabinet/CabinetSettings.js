6e32f536a712e9586dfd24093c2d0119c1eb12c1
Artur Lihitskiy <artur.lihitskiy@gmail.com>
2024-04-14 21:04:58 +0300

    Add new visual functional to settings

--- web/src/components/cabinet/CabinetSettings.js
+++ web/src/components/cabinet/CabinetSettings.js
@@ -18,9 +18,20 @@ const CabinetSettings = () => {
         <li>
           <h3>Ім'я</h3>
           <div className={styles.settingContainer}>
-            <span>Артур Ліхіцький</span>
-            <input type="text" placeholder="Нове ім'я" />
-            <button>
+            <span className={isNameChanged && styles.hide}>
+              Артур Ліхіцький
+            </span>
+            <input
+              className={isNameChanged && styles.active}
+              type="text"
+              placeholder="Нове ім'я"
+            />
+            <button
+              className={isNameChanged && styles.active}
+              onClick={() => {
+                setIsNameChanged(!isNameChanged);
+              }}
+            >
               <PencilIcon />
             </button>
           </div>
@@ -28,9 +39,20 @@ const CabinetSettings = () => {
         <li>
           <h3>Пошта</h3>
           <div className={styles.settingContainer}>
-            <span>artur.lihitskiy@gmail.com</span>
-            <input type="text" placeholder="Нова пошта" />
-            <button>
+            <span className={isEmailChanged && styles.hide}>
+              artur.lihitskiy@gmail.com
+            </span>
+            <input
+              className={isEmailChanged && styles.active}
+              type="text"
+              placeholder="Нова пошта"
+            />
+            <button
+              className={isEmailChanged && styles.active}
+              onClick={() => {
+                setIsEmailChanged(!isEmailChanged);
+              }}
+            >
               <PencilIcon />
             </button>
           </div>
@@ -38,14 +60,31 @@ const CabinetSettings = () => {
         <li>
           <h3>Пароль</h3>
           <div className={styles.settingContainer}>
-            <span>1234312</span>
-            <input type="text" placeholder="Новий пароль" />
-            <button>
+            <span className={isPasswordChanged && styles.hide}>qwerasdzxc</span>
+            <input
+              className={isPasswordChanged && styles.active}
+              type="text"
+              placeholder="Новий пароль"
+            />
+            <button
+              className={isPasswordChanged && styles.active}
+              onClick={() => {
+                setIsPasswordChanged(!isPasswordChanged);
+              }}
+            >
               <PencilIcon />
             </button>
           </div>
         </li>
       </ul>
+      <button
+        className={`${styles.saveButton} ${
+          (isNameChanged || isEmailChanged || isPasswordChanged) &&
+          styles.active
+        }`}
+      >
+        Зберегти
+      </button>
     </>
   );
 };
